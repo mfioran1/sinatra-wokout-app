73bf06b730f1ec39bf81a1bef0b96517e6d36c7c
mfioran1 <maurice97f@gmail.com>
2020-05-12 19:00:09 -0700

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,52 @@
 class UsersController < ApplicationController
 
+    #Signup page
+    get '/signup' do
+        if logged_in?
+            redirect to '/workouts'
+        else
+            erb :'/users/create'
+        end
+    end
+
+    #signup post
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            @user = User.create(
+                :username => params[:username]
+                :email => params[:email]
+                :password => params[:password]
+            )
+            session[:user_id] = @user.id 
+            redirect to '/workouts'
+        end
+    end
+
+    #login page
+    get '/login' do
+      if logged_in
+        @user = current_user
+        redirect to "/users/#{@user.slug}"
+      else
+        erb :"/users/login"
+      end
+    end  
+
+    #login post
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to "/users/#{@user.slug}"
+        else
+            flash[:message] = "***Incorrect username or password, please try again!***"
+            erb :"/users/login"
+        end
+    end
+
+    #show page
     
 
 end
--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -1,3 +1,13 @@
 class WorkoutController < ApplicationController
 
+    get '/workouts' do
+        if logged_in?
+            @user = current_user
+            erb :'/workouts/index'
+        else
+            redirect to '/login'
+        end
+    end
+
+
 end
